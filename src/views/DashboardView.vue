5a180cd083b614274f7d24787c4ed37150db0b22
dlevak92 <dino-9@gmail.com>
2023-08-08 20:37:44 +0200

    Syling i opcije prikaza kartica

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -5,18 +5,21 @@
   <div>
     <h2>Welcome to Fipuzor Card Dashboard</h2>
 
-    <!-- Informacije o karticama -->
+      <!--Kontenjer za prikaz kartice - @click event-->
     <div class="card-container">
-      <div v-for="(card, index) in cards" :key="index" class="loyalty-card">
+      <div
+        v-for="(card, index) in cards"
+        :key="index"
+        :class="{ 'loyalty-card': true, 'expanded': card.expanded }"
+        @click="toggleCardExpansion(card)"
+        >
         <img :src="card.logo" alt="Card Logo" class="card-logo">
         <h3>{{ card.name }} Card</h3>
         <p>Card Number: {{ card.cardNumber }}</p>
         <p>Expire Date: {{ card.expireDate }}</p>
-        <!-- Add more loyalty card information here -->
       </div>
     </div>
 
-    <!-- Test botun za dodavanje kartica -->
     <button class="btn btn-success btn-circle" @click="addCard"><i class="bi bi-plus"></i></button>
   </div>
 </template>
@@ -30,15 +33,17 @@ export default {
           name: "Lidl",
           cardNumber: "1234-5678-9012",
           expireDate: "12/25",
-          logo: "../assets/fipuzor_logo.png", //Path prema folderu s "defaultnim" logo-ima trgovina
+          logo: "../assets/fipuzor_logo.png",
+          expanded: false,
         },
         {
           name: "Pevex",
           cardNumber: "9012-8765-1243",
           expireDate: "05/24",
-          logo: "../assets/fipuzor_logo.png", //Path prema folderu s "defaultnim" logo-ima trgovina
+          logo: "../assets/fipuzor_logo.png",
+          expanded: false,
         },
-        //Dodaj jos testnih kartica za pregled izgleda
+        // Add more card objects here
       ],
     };
   },
@@ -46,14 +51,21 @@ export default {
     addCard() {
       // Add card komponenta
     },
+    //toggle na klik (@click eventom), card.expanded se mjenja klikanjem (pomocu ! operatora) 
+    toggleCardExpansion(card) {
+      card.expanded = !card.expanded;
+    },
   },
 };
 </script>
 
-<!-- Dizajn ya prikay kartica -->
+<!--.cards-container kontenjer za storanje kartica-->
+<!--Display:flex poslozi kartice u red, overflow-x: auto skrolanje preko sirine kontenjera-->
+<!--.loyalty-card kontenjer za jednu karticu (cursor: pointer mjenja kursor kod hoveranja iznad kartice)-->
+<!--.loyalty-card.ecpanded stajling kartice nakon expanda-->
 <style scoped>
 .card-container {
-  display: flex;
+  display: flex; 
   flex-wrap: nowrap;
   overflow-x: auto;
   padding: 1rem;
@@ -67,11 +79,18 @@ export default {
   width: 300px;
   flex-shrink: 0;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
 }
 
+.loyalty-card.expanded {
+  width: 500px;
+}
+/**
 .card-logo {
   max-width: 100px;
   height: auto;
   margin-bottom: 1rem;
 }
+*/
 </style>
