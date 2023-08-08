28b58a0c78ea0bf3c9a925b0125ed888e5d3b2a2
dlevak92 <dino-9@gmail.com>
2023-08-08 20:01:27 +0200

    Rad na dashboardu, AddCard komponenta, uredjivanje

---
+++ src/components/AddCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <h3>Add New Card</h3>
+      <form @submit.prevent="submitForm">
+        <label for="newCardNumber">Card Number:</label>
+        <input v-model="newCardNumber" id="newCardNumber" type="text" required>
+        <button type="submit">Add Card</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newCardNumber: "",
+      };
+    },
+    methods: {
+      submitForm() {
+        // Validate and process the new card number
+        // Update the cardNumber and points data in the parent component
+        this.$emit("card-added", this.newCardNumber);
+        this.newCardNumber = ""; // Reset input
+      },
+    },
+  };
+  </script>
+  
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,38 +1,77 @@
 <template>
-      <div class="home">
+  <div class="home">
     <img alt="Fipuzor Logo" src="../assets/logo_fipuzor_smaller.png">
   </div>
-    <div>
-      <h2>Welcome to Fipuzor Card Dashboard</h2>
-  
-      <!-- Informacije o karticama -->
-      <div>
-        <h3>Fipuzor Card Information</h3>
-        <p>Card Number: {{ cardNumber }}</p>
-        <p>Points: {{ points }}</p>
+  <div>
+    <h2>Welcome to Fipuzor Card Dashboard</h2>
+
+    <!-- Informacije o karticama -->
+    <div class="card-container">
+      <div v-for="(card, index) in cards" :key="index" class="loyalty-card">
+        <img :src="card.logo" alt="Card Logo" class="card-logo">
+        <h3>{{ card.name }} Card</h3>
+        <p>Card Number: {{ card.cardNumber }}</p>
+        <p>Expire Date: {{ card.expireDate }}</p>
         <!-- Add more loyalty card information here -->
       </div>
-  
-      <!-- Test botun za dodavanje kartica -->
-      <button class="btn btn-success" @click="addCard">Add Card</button>
-  
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        cardNumber: "1234-5678-9012", //Testni prikaz kartice
-        points: 500 //Testni prikaz bodova
-      };
+
+    <!-- Test botun za dodavanje kartica -->
+    <button class="btn btn-success btn-circle" @click="addCard"><i class="bi bi-plus"></i></button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cards: [
+        {
+          name: "Lidl",
+          cardNumber: "1234-5678-9012",
+          expireDate: "12/25",
+          logo: "../assets/fipuzor_logo.png", //Path prema folderu s "defaultnim" logo-ima trgovina
+        },
+        {
+          name: "Pevex",
+          cardNumber: "9012-8765-1243",
+          expireDate: "05/24",
+          logo: "../assets/fipuzor_logo.png", //Path prema folderu s "defaultnim" logo-ima trgovina
+        },
+        //Dodaj jos testnih kartica za pregled izgleda
+      ],
+    };
+  },
+  methods: {
+    addCard() {
+      // Add card komponenta
     },
-    methods: {
-      addCard() {
-        //Dodavanje kartice
-        //Otvaranje "add card" view-a?
-      }
-    }
-  };
-  </script>
-  
+  },
+};
+</script>
+
+<!-- Dizajn ya prikay kartica -->
+<style scoped>
+.card-container {
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+  padding: 1rem;
+}
+
+.loyalty-card {
+  background-color: #f8f9fa;
+  border-radius: 8px;
+  padding: 1rem;
+  margin-right: 1rem;
+  width: 300px;
+  flex-shrink: 0;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.card-logo {
+  max-width: 100px;
+  height: auto;
+  margin-bottom: 1rem;
+}
+</style>
