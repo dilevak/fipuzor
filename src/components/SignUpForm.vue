a7ebb70964de753033c3c10df9ff455b8f833d4a
dlevak92 <dino-9@gmail.com>
2023-08-12 10:54:48 +0200

    Razvoj signup komponente, preprada signupview-a

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,67 @@
+<template>
+    <form name="signup-form">
+      <div class="mb-3">
+        <label for="username">Username: </label>
+        <input type="text" id="username" v-model="input.username" />
+      </div>
+      <div class="mb-3">
+        <label for="email">Email: </label>
+        <input type="email" id="email" v-model="input.email" />
+      </div>
+      <div class="mb-3">
+        <label for="password">Password: </label>
+        <input type="password" id="password" v-model="input.password" />
+      </div>
+      <button class="btn btn-outline-success" type="submit" @click.prevent="signup()">
+        Signup
+      </button>
+      <p>Already have an account? <router-link to="/login" class="btn btn-primary">Login</router-link></p>
+      
+      <p v-if="signupStatusMessage">{{ signupStatusMessage }}</p>
+    </form>
+  </template>
+  
+  <script>
+import axios from 'axios';
+
+export default {
+  name: 'SignUpForm',
+  data() {
+    return {
+      input: {
+        username: '',
+        email: '',
+        password: '',
+      },
+      signupStatusMessage: '',
+    };
+  },
+  methods: {
+    async signup() {
+      try {
+        if (this.input.username !== '' && this.input.email !== '' && this.input.password !== '') {
+          const response = await axios.post('http://localhost:3000/api/signup', {
+            username: this.input.username,
+            email: this.input.email,
+            password: this.input.password,
+          });
+
+          if (response.data.success) {
+            console.log('User registered');
+            this.signupStatusMessage = 'User registered';
+          } else {
+            console.log('Registration failed');
+            this.signupStatusMessage = 'Registration failed';
+          }
+        } else {
+          console.log('Username, Email, and Password cannot be empty');
+          this.signupStatusMessage = 'Username, Email, and Password cannot be empty';
+        }
+      } catch (error) {
+        console.error('An error occurred:', error);
+        this.signupStatusMessage = 'An error occurred';
+      }
+    },
+  },
+};
+</script>
---
+++ src/views/SignupView copy.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="home">
+    <img alt="Fipuzor Logo" src="../assets/logo_fipuzor.png">
+  </div>
+    <!--Forma za signup i signup botun-->
+   <!--Pomocu v-modela cemo bindati input i moći ćemo pohraniti te varijable i pristupiti im kroz kod.-->
+  <form name="signup-form">
+    <div class="mb-3">
+      <label for="email">Email: </label>
+      <input type="email" id="email" v-model="input.email" />
+    </div>
+    <div class="mb-3">
+      <label for="password">Password: </label>
+      <input type="password" id="password" v-model="input.password" />
+    </div>
+    <button class="btn btn-outline-success" type="submit" @click.prevent="signup()">
+      Signup
+    </button>
+    <p>Already have an account? <router-link to="/login" class="btn btn-primary">Login</router-link></p>
+    
+    <!-- Test for printing email/password variables -->
+    <!--<p>Email is: {{ input.email }}</p> -->
+    <!--<p>Password is: {{ input.password }}</p> -->
+
+    <!-- Display signup status message to the user -->
+    <p v-if="signupStatusMessage">{{ signupStatusMessage }}</p>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'SignupView',
+  data() {
+    return {
+      input: {
+        email: '',
+        password: '',
+      },
+      signupStatusMessage: '', // To store the signup status message
+    };
+  },
+  methods: {
+    signup() {
+      // Make sure both email and password are not empty
+      if (this.input.email !== '' && this.input.password !== '') {
+        console.log('registered');
+        this.signupStatusMessage = ''; // Don't display the "registered" status to the user
+      } else {
+        console.log('Email and Password cannot be empty');
+        this.signupStatusMessage = 'Email and Password cannot be empty'; // Error message displayed to the user
+      }
+    },
+  },
+};
+</script>
